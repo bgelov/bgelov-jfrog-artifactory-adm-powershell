c6b777d8d735e60f9900fcb79daaa61072fcf84c
Oleg Belov <belov@ymail.com>
2022-05-20 18:54:59 +0300

    Create Import ldap group to artifactory.ps1

---
+++ Import ldap group to artifactory.ps1
@@ -0,0 +1,24 @@
+﻿#Import LDAP group to Artifactory
+
+$apiKey = 'SDASDADASDASADASDASDASDASDASDASDASDASDASDASDASDASDASDASD'
+$site = 'https://artifactory.myartifactory.ru'
+
+$groupName = 'artifactory-nuget-group' #group name (in lower case)
+$group = Get-ADGroup $groupName -Properties DistinguishedName,Description
+
+$groupDn = $group.DistinguishedName
+$groupDescription = $group.Description
+
+$contentType = "application/json"      
+$headers = @{'X-JFrog-Art-Api' = $apiKey}
+
+$data = @{        
+    name = $groupName;
+    description = $groupDescription;
+    autoJoin = "false";
+    realm = "ldap";
+    realmAttributes = "ldapGroupName=$groupName;groupsStrategy=STATIC;groupDn=$groupDn"
+};
+$json = $data | ConvertTo-Json;
+Invoke-RestMethod -H $headers -Method PUT -Uri "$site/api/security/groups/$groupName" -ContentType $contentType -Body $json
+
